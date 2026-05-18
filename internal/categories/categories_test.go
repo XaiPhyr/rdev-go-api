04ecd926d2c356cba4e833c6682ac209f599c5fe
XaiPhyr <xaiphyrrr@gmail.com>
2026-05-18 15:53:07 +0800

    chore: added category test func

--- internal/categories/categories_test.go
+++ internal/categories/categories_test.go
@@ -11,16 +11,27 @@ import (
 )
 
 type CategoryTest struct {
-	GetCategoryByUUIDFunc func(ctx context.Context, uuid string) (*models.Category, error)
-	GetCategoriesFunc     func(ctx context.Context, q dto.BaseFilters) ([]models.Category, int, error)
-	GetCategoryTreeFunc   func(ctx context.Context, q dto.BaseFilters) ([]categories.CategoryTree, error)
-	CreateCategoryFunc    func(ctx context.Context, category *models.Category) error
+	GetCategoryByUUIDFunc    func(ctx context.Context, uuid string) (*models.Category, error)
+	GetCategoriesFunc        func(ctx context.Context, q dto.BaseFilters) ([]models.Category, int, error)
+	GetCategoryTreeFunc      func(ctx context.Context, q dto.BaseFilters) ([]categories.CategoryTree, error)
+	CreateCategoryFunc       func(ctx context.Context, category *models.Category) error
+	UpdateCategoryFunc       func(ctx context.Context, category *models.Category) error
+	DeleteCategoryFunc       func(ctx context.Context, uuid string) error
+	UpdateCategoryStatusFunc func(ctx context.Context, uuid string) error
 }
 
 func (m *CategoryTest) CreateCategory(ctx context.Context, category *models.Category) error {
+	if m.CreateCategoryFunc != nil {
+		return m.CreateCategoryFunc(ctx, category)
+	}
+
 	return nil
 }
 func (m *CategoryTest) GetCategoryByUUID(ctx context.Context, uuid string) (*models.Category, error) {
+	if m.GetCategoryByUUIDFunc != nil {
+		return m.GetCategoryByUUIDFunc(ctx, uuid)
+	}
+
 	return nil, nil
 }
 func (m *CategoryTest) GetCategories(ctx context.Context, q dto.BaseFilters) ([]models.Category, int, error) {
@@ -31,15 +42,30 @@ func (m *CategoryTest) GetCategories(ctx context.Context, q dto.BaseFilters) ([]
 	return nil, 0, nil
 }
 func (m *CategoryTest) GetCategoryTree(ctx context.Context, q dto.BaseFilters) ([]categories.CategoryTree, error) {
+	if m.GetCategoryTreeFunc != nil {
+		return m.GetCategoryTreeFunc(ctx, q)
+	}
+
 	return nil, nil
 }
 func (m *CategoryTest) UpdateCategory(ctx context.Context, category *models.Category) error {
+	if m.UpdateCategoryFunc != nil {
+		return m.UpdateCategoryFunc(ctx, category)
+	}
 	return nil
 }
 func (m *CategoryTest) DeleteCategory(ctx context.Context, uuid string) error {
+	if m.DeleteCategoryFunc != nil {
+		return m.DeleteCategoryFunc(ctx, uuid)
+	}
+
 	return nil
 }
 func (m *CategoryTest) UpdateCategoryStatus(ctx context.Context, uuid string) error {
+	if m.UpdateCategoryStatusFunc != nil {
+		return m.UpdateCategoryStatusFunc(ctx, uuid)
+	}
+
 	return nil
 }
 
@@ -57,7 +83,7 @@ func TestGetCategories(t *testing.T) {
 		return []models.Category{{Name: "Test Category"}}, 1, nil
 	}
 
-	query := dto.Query{Search: "1"}
+	query := dto.Query{Search: "test"}
 	_, _, err := testCategorySvc.GetCategories(context.Background(), query)
 
 	if err != nil {
