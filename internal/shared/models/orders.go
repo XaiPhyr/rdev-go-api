a5175ec76934ff213b2861ea0ef8fefebb4a6345
XaiPhyr <xaiphyrrr@gmail.com>
2026-05-22 21:00:31 +0800

    chore: added OrderItem struct

--- internal/shared/models/orders.go
+++ internal/shared/models/orders.go
@@ -9,9 +9,20 @@ type Order struct {
 	bun.BaseModel `bun:"table:orders,alias:o"`
 	fields.BaseFields
 
-	ParentID    int64  `bun:"-" json:"parent_id,omitempty"`
-	CustomerID  int64  `bun:"customer_id" json:"customer_id"`
-	OrderNumber string `bun:"order_number" json:"order_number"`
-	OrderStatus string `bun:"order_status" json:"order_status"`
-	TotalAmount int64  `bun:"total_amount" json:"total_amount"`
+	CustomerID    int64       `bun:"customer_id" json:"customer_id" validate:"required"`
+	OrderNumber   string      `bun:"order_number" json:"order_number"`
+	OrderStatus   string      `bun:"order_status" json:"order_status"`
+	PaymentStatus string      `bun:"payment_status" json:"payment_status"`
+	TotalAmount   int64       `bun:"total_amount" json:"total_amount"`
+	OrderItem     []OrderItem `bun:"rel:has-many,join:id=order_id" json:"order_item,omitempty"`
+}
+
+type OrderItem struct {
+	bun.BaseModel `bun:"table:order_items,alias:oi"`
+	fields.BaseFields
+
+	OrderID          int64 `bun:"order_id" json:"order_id"`
+	ProductID        int64 `bun:"product_id" json:"product_id"`
+	TransactionPrice int64 `bun:"transaction_price" json:"transaction_price"`
+	Quantity         int64 `bun:"quantity" json:"quantity"`
 }
